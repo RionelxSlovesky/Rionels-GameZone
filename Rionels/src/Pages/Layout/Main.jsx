60a239536861f66e2e502dd74766d62262d5cea4
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-21 08:06:30 +0600

    Customized Footer
    Footer used to be fixed to the bottom, which looked bad and took away a lot of the screen. Therefore, made it stay at the bottom but not with scroll.

--- Rionels/src/Pages/Layout/Main.jsx
+++ Rionels/src/Pages/Layout/Main.jsx
@@ -5,10 +5,12 @@ import Footer from "../Shared/Footer/Footer";
 
 const Main = () => {
     return (
-        <div className="max-w-[1600px] mx-auto">
+        <div className="max-w-[1600px] mx-auto min-h-screen flex flex-col">
             <Header></Header>
             <Outlet></Outlet>
+        <div className="mt-auto">
             <Footer></Footer>
+        </div>
         </div>
     );
 };
