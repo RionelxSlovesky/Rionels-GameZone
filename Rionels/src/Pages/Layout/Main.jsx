f9a6f0f375377af7781d6db512306f74eb933f43
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-21 10:30:21 +0600

    Updated Footer and Header
    Now their width takes full screen

--- Rionels/src/Pages/Layout/Main.jsx
+++ Rionels/src/Pages/Layout/Main.jsx
@@ -2,17 +2,18 @@ import { Outlet } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import Footer from "../Shared/Footer/Footer";
 
-
 const Main = () => {
-    return (
-        <div className="max-w-[1600px] mx-auto min-h-screen flex flex-col">
-            <Header></Header>
-            <Outlet></Outlet>
-        <div className="mt-auto">
-            <Footer></Footer>
-        </div>
-        </div>
-    );
+  return (
+    <>
+      <Header></Header>
+      <div className="max-w-[1600px] mx-auto min-h-screen flex flex-col">
+        <Outlet></Outlet>
+      </div>
+      <div className="mt-auto">
+        <Footer></Footer>
+      </div>
+    </>
+  );
 };
 
 export default Main;
